f8197b44a24b393acbef3a84d57754f60bf59d98
prosodyspeaks <prosodyspeaks@gmail.com>
2023-12-04 05:35:36 +0000

    rename

--- src/gurupod/episodes.py
+++ src/gurupod/episodes.py
@@ -21,7 +21,7 @@ def ep_soup_date(ep_soup) -> datetime.date:
 
 
 def ep_soup_notes(soup: BeautifulSoup) -> list:
-    ''' some listing have literal("Links") some dont '''
+    ''' some listing have literal("Links") as heading for next section some dont '''
 
     paragraphs = soup.select(".show-notes p")
     show_notes = [p.text for p in paragraphs if p.text != "Links"]
@@ -35,7 +35,7 @@ def ep_soup_links(soup: BeautifulSoup) -> dict:
     return show_links_dict
 
 
-async def names_n_links_as(page_url: str, session):
+async def names_n_links(page_url: str, session):
     async with session.get(page_url) as response:
         text = await response.text()
     soup = BeautifulSoup(text, "html.parser")
@@ -73,50 +73,50 @@ class Episode:
         )
 
 
-async def episodes_from_page_as(
+async def episodes_from_page(
         page_url: str, session, existing_eps: dict or None = None) -> List[Episode]:
     existing_eps = existing_eps or {}
     new_eps = []
-    async for tup in names_n_links_as(page_url, session):
+    async for tup in names_n_links(page_url, session):
         if tup[0] in existing_eps:
             print(f"Already Exists: {tup[0]}")
             return new_eps
 
         print(f"New episode found: {tup[0]}")
-        ep_soup = await ep_soup_from_link_as(tup[1], session)
+        ep_soup = await ep_soup_from_link(tup[1], session)
         new_eps.append(Episode.from_tup_n_soup(tup, ep_soup))
 
     return new_eps
 
 
-async def ep_soup_from_link_as(link, session) -> BeautifulSoup:
+async def ep_soup_from_link(link, session) -> BeautifulSoup:
     async with session.get(link) as response:
         text = await response.text()
         return BeautifulSoup(text, "html.parser")
 
 
-async def get_new_eps_as(main_url: str, existing_eps: dict or None = None) -> List[Episode]:
+async def get_new_eps(main_url: str, existing_eps: dict or None = None) -> List[Episode]:
     episodes = []
     async with aiohttp.ClientSession() as session:
-        pages = await get_listing_pages_as(main_url, session)
+        pages = await get_listing_pages(main_url, session)
         for page in pages:
-            eps = await episodes_from_page_as(page, session, existing_eps)
+            eps = await episodes_from_page(page, session, existing_eps)
             if not eps:
                 break
             episodes.extend(eps)
     return episodes
 
 
-async def get_listing_pages_as(main_url: str, session) -> List[str]:
-    num_pages = await _get_num_pages_as(main_url, session)
-    return [_url_from_pagenum_as(main_url, page_num) for page_num in range(num_pages)]
+async def get_listing_pages(main_url: str, session) -> List[str]:
+    num_pages = await _get_num_pages(main_url, session)
+    return [_url_from_pagenum(main_url, page_num) for page_num in range(num_pages)]
 
 
-def _url_from_pagenum_as(main_url: str, page_num: int) -> str:
+def _url_from_pagenum(main_url: str, page_num: int) -> str:
     return main_url + f"/episodes/{page_num + 1}/#showEpisodes"
 
 
-async def _get_response_as(url: str, session):
+async def _get_response(url: str, session):
     for _ in range(3):
         try:
             async with session.get(url) as response:
@@ -130,8 +130,8 @@ async def _get_response_as(url: str, session):
         raise aiohttp.ClientError("Request failed 3 times")
 
 
-async def _get_num_pages_as(main_url: str, session) -> int:
-    response = await _get_response_as(main_url, session)
+async def _get_num_pages(main_url: str, session) -> int:
+    response = await _get_response(main_url, session)
     soup = BeautifulSoup(response, "html.parser")
     page_links = soup.select(".page-link")
     lastpage = page_links[-1]['href']
@@ -141,7 +141,7 @@ async def _get_num_pages_as(main_url: str, session) -> int:
 
 def get_eps():
     existing_dict, existing_eps = existing_eps_()
-    new_eps = asyncio.run(get_new_eps_as(MAIN_URL, existing_eps=existing_dict))
+    new_eps = asyncio.run(get_new_eps(MAIN_URL, existing_eps=existing_dict))
     all_eps = existing_eps + new_eps
     if new_eps:
         eps_to_json_(all_eps)
