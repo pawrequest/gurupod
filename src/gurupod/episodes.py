0e9971d5e969fcb05f2c5a949a14c3b3234df9e9
prosodyspeaks <prosodyspeaks@gmail.com>
2023-12-04 05:13:17 +0000

    tidy

--- src/gurupod/episodes.py
+++ src/gurupod/episodes.py
@@ -1,86 +1,3 @@
-# import asyncio
-# from typing import List
-#
-# import aiohttp
-# from bs4 import BeautifulSoup
-#
-# from gurupod.episode import Episode
-#
-#
-# async def names_n_links_as(page_url: str):
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get(page_url) as response:
-#             text = await response.text()
-#     soup = BeautifulSoup(text, "html.parser")
-#     episode_soup = soup.select(".episode")
-#     for episode in episode_soup:
-#         yield (episode.select_one(".episode-title a").text,
-#                str(episode.select_one(".episode-title a")['href']))
-#
-#
-# async def episodes_from_page_as(
-#         page_url: str, session, existing_eps: dict or None = None) -> List[Episode]:
-#     existing_eps = existing_eps or {}
-#     new_eps = []
-#     async for tup in names_n_links_as(page_url):
-#         if tup[0] in existing_eps:
-#             print(f"Already Exists: {tup[0]}")
-#             return new_eps
-#
-#         print(f"New episode found: {tup[0]}")
-#         ep_soup = await ep_soup_from_link_as(tup[1], session)
-#         new_eps.append(Episode.from_tup_n_soup(tup, ep_soup))
-#
-#     return new_eps
-#
-#
-# async def ep_soup_from_link_as(link, session) -> BeautifulSoup:
-#     async with session.get(link) as response:
-#         text = await response.text()
-#         return BeautifulSoup(text, "html.parser")
-#
-#
-# async def get_all_eps_as(main_url: str, existing_eps: dict or None = None) -> List[Episode]:
-#     episodes = []
-#     async with aiohttp.ClientSession() as session:
-#         pages = await get_listing_pages_as(main_url)
-#         for page in pages:
-#             eps = await episodes_from_page_as(page, session, existing_eps)
-#             if not eps:
-#                 break
-#             episodes.extend(eps)
-#     return episodes
-#
-#
-# async def get_listing_pages_as(main_url: str) -> List[str]:
-#     num_pages = _get_num_pages_as(main_url)
-#     return [_url_from_pagenum_as(main_url, page_num) for page_num in range(num_pages)]
-#
-#
-# def _url_from_pagenum_as(main_url: str, page_num: int) -> str:
-#     return main_url + f"/episodes/{page_num + 1}/#showEpisodes"
-#
-#
-# async def _get_response_as(url: str, session):
-#     for _ in range(3):
-#         try:
-#             async with session.get(url) as response:
-#                 response.raise_for_status()
-#                 return await response.text()
-#         except aiohttp.ClientError as e:
-#             print(f"Request failed: {e}")
-#             await asyncio.sleep(2)
-#             continue
-#     else:
-#         raise aiohttp.ClientError("Request failed 3 times")
-#
-# async def _get_num_pages_as(main_url: str, session) -> int:
-#     response = await _get_response_as(main_url, session)
-#     soup = BeautifulSoup(response, "html.parser")
-#     page_links = soup.select(".page-link")
-#     lastpage = page_links[-1]['href']
-#     num_pages = lastpage.split("/")[-1].split("#")[0]
-#     return int(num_pages)
 from __future__ import annotations
 
 import asyncio
@@ -92,9 +9,30 @@ from typing import List
 
 import aiohttp
 from bs4 import BeautifulSoup
+from dateutil import parser
 
 from gurupod.data.consts import EPISODES_JSON, MAIN_URL
-from gurupod.episode_sync import ep_soup_date, ep_soup_links, ep_soup_notes
+
+
+def ep_soup_date(ep_soup) -> datetime.date:
+    date_str = ep_soup.select_one(".publish-date").text
+    datey = parser.parse(date_str)
+    return datey.date()
+
+
+def ep_soup_notes(soup: BeautifulSoup) -> list:
+    ''' some listing have literal("Links") some dont '''
+
+    paragraphs = soup.select(".show-notes p")
+    show_notes = [p.text for p in paragraphs if p.text != "Links"]
+
+    return show_notes
+
+
+def ep_soup_links(soup: BeautifulSoup) -> dict:
+    show_links_html = soup.select(".show-notes a")
+    show_links_dict = {aref.text: aref['href'] for aref in show_links_html}
+    return show_links_dict
 
 
 async def names_n_links_as(page_url: str, session):
