163dc26426a012da8189cbc8a67da2681509710f
prosodyspeaks <prosodyspeaks@gmail.com>
2023-12-04 02:29:35 +0000

    Episode.from_tup_n_soup

--- src/gurupod/episode.py
+++ src/gurupod/episode.py
@@ -19,13 +19,13 @@ class Episode:
     show_url: str
 
     @classmethod
-    def from_sub_soup(cls, ep_soup) -> Episode:
+    def from_tup_n_soup(cls, ep_tup, ep_soup) -> Episode:
         return cls(
-            show_name=ep_soup.select_one(".episode-title a").text,
-            show_date=ep_soup.select_one(".publish-date").text,
-            show_url=ep_soup.select_one(".episode-title a")['href'],
-            show_notes=notes_from_soup(ep_soup),
-            show_links=links_from_soup(ep_soup),
+            show_name=ep_tup[0],
+            show_url=ep_tup[1],
+            show_date=ep_soup_date(ep_soup),
+            show_notes=ep_soup_notes(ep_soup),
+            show_links=ep_soup_links(ep_soup),
         )
 
 
@@ -42,21 +42,32 @@ def get_all_episodes(main_url: str, existing_eps: dict or None = None) -> List[E
 def episodes_from_listing_page(page_url: str, existing_eps: dict or None = None) -> List[Episode]:
     existing_eps = existing_eps or {}
     new_eps = []
-    ep_pages = listing_page_to_episode_pages(page_url)
-    for title, link in ep_pages:
-        if title in existing_eps:
-            print(f"Already Exists: {title}")
+    ep_tups = list_page_names_links(page_url)
+    for tup in ep_tups:
+        if tup[0] in existing_eps:
+            print(f"Already Exists: {tup[0]}")
             return new_eps
 
-        print(f"New episode found: {title}")
-        ep_soup = episode_soup_from_link(link)
-        new_eps.append(Episode(
-            show_name=title,
-            show_url=link,
-            show_date=date_from_soup(ep_soup),
-            show_notes=notes_from_soup(ep_soup),
-            show_links=links_from_soup(ep_soup),
-        ))
+        print(f"New episode found: {tup[0]}")
+        ep_soup = ep_soup_from_link(tup[1])
+        new_eps.append(Episode.from_tup_n_soup(tup, ep_soup))
+
+    #
+    # for title, link in ep_tups:
+    #     if title in existing_eps:
+    #         print(f"Already Exists: {title}")
+    #         return new_eps
+    #
+    #     print(f"New episode found: {title}")
+    #     ep_soup = ep_soup_from_link(link)
+    #     new
+    #     new_eps.append(Episode(
+    #         show_name=title,
+    #         show_url=link,
+    #         show_date=ep_soup_date(ep_soup),
+    #         show_notes=ep_soup_notes(ep_soup),
+    #         show_links=ep_soup_links(ep_soup),
+    #     ))
 
     return new_eps
 
@@ -103,35 +114,28 @@ def _url_from_pagenum(main_url: str, page_num: int) -> str:
 # Soupy functions:
 
 
-def episodes_soup_from_page(page_url):
-    response = _get_response(page_url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    return soup.select(".episode")
-
-
-def listing_page_to_episode_pages(page_url: str) -> tuple[tuple[str, str]]:
+def list_page_names_links(page_url: str) -> tuple[tuple[str, str]]:
     response = requests.get(page_url)
     soup = BeautifulSoup(response.text, "html.parser")
     episode_soup = soup.select(".episode")
-    epsgen = ((episode.select_one(".episode-title a").text,
-               str(episode.select_one(".episode-title a")['href'])) for episode in episode_soup)
+    episodes = ((episode.select_one(".episode-title a").text,
+                 str(episode.select_one(".episode-title a")['href'])) for episode in episode_soup)
 
-    eps_tup = tuple(epsgen)
-    return eps_tup
+    return tuple(episodes)
 
 
-def episode_soup_from_link(link) -> BeautifulSoup:
+def ep_soup_from_link(link) -> BeautifulSoup:
     response = _get_response(link)
     return BeautifulSoup(response.text, "html.parser")
 
 
-def date_from_soup(ep_soup) -> datetime.date:
+def ep_soup_date(ep_soup) -> datetime.date:
     date_str = ep_soup.select_one(".publish-date").text
     datey = parser.parse(date_str)
     return datey.date()
 
 
-def notes_from_soup(soup: BeautifulSoup) -> list:
+def ep_soup_notes(soup: BeautifulSoup) -> list:
     ''' some listing have literal("Links") some dont '''
 
     paragraphs = soup.select(".show-notes p")
@@ -140,7 +144,7 @@ def notes_from_soup(soup: BeautifulSoup) -> list:
     return show_notes
 
 
-def links_from_soup(soup: BeautifulSoup) -> dict:
+def ep_soup_links(soup: BeautifulSoup) -> dict:
     show_links_html = soup.select(".show-notes a")
     show_links_dict = {aref.text: aref['href'] for aref in show_links_html}
     return show_links_dict
