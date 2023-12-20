7e62cde1a251f81e1b218bf056feb1abcdda5caa
paw <paw@pawrequest.com>
2023-12-20 03:42:02 +0000

    move logs

--- src/gurupod/gurulog.py
+++ src/gurupod/gurulog.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from loguru import logger
 # log_file_loc = "../../src/data/logs/gurulog.log"
-log_file_loc = Path(__file__).parent.parent / 'data' / 'logs' / 'gurulog.log'
+log_file_loc = Path(__file__).parent.parent.parent / 'data' / 'logs' / 'gurulog.log'
 
 logger.add(log_file_loc, rotation="1 day", delay=True)
 logger.info(f'Starting logger {log_file_loc}')
