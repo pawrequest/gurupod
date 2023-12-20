2ca119de6c0274fae01a7fa44a0b911790237a07
paw <paw@pawrequest.com>
2023-12-20 01:29:43 +0000

    oops, smash, deploy?

--- src/gurupod/gurulog.py
+++ src/gurupod/gurulog.py
@@ -1,3 +1,9 @@
+import os.path
+from pathlib import Path
+
 from loguru import logger
-log_file_loc = "../src/data/logs/gurulog.log"
+# log_file_loc = "../../src/data/logs/gurulog.log"
+log_file_loc = Path(__file__).parent.parent / 'data' / 'logs' / 'gurulog.log'
+
 logger.add(log_file_loc, rotation="1 day", delay=True)
+logger.info(f'Starting logger {log_file_loc}')
